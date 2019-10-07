f5780177b3fc8cac762d3149f494f69f88ebd80e
0x6d696368 <>
2019-10-07 09:24:59 +0200

    Fix: exported whitelist function

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="threatrack_iocextract",
-	version="0.0.5",
+	version="0.0.6",
 	author="",
 	author_email="",
 	description="IOC extractor",
--- threatrack_iocextract/__init__.py
+++ threatrack_iocextract/__init__.py
@@ -1,4 +1,4 @@
-from threatrack_iocextract.iocextract import load_patterns, refang, extract, extract_all
+from threatrack_iocextract.iocextract import load_patterns, refang, extract, extract_all, whitelist
 
 name = "threatrack_iocextract"
 
